ae1b171c86beba411eda2efe0cd0014142ffeb51
Anna-MariaFilipova <58307360+Anna-MariaFilipova@users.noreply.github.com>
2020-07-13 17:00:40 +0300

    Update Checks.java

--- Checks.java
+++ Checks.java
@@ -3,95 +3,86 @@ package mobileOperator;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Date;
-import java.util.Scanner;
 
 public class Checks {
 
-	public static  void checkForAdmin() {
-		
-		System.out.println("Enter name,email and password:");
-		Scanner scanner = new Scanner(System.in);
-		String searchedName = scanner.nextLine();
-	    String searchedEmail = scanner.nextLine();
-	    String searchedPassword = scanner.nextLine();
-	    
-	 try {
-	     
-	 String query = "SELECT * FROM admins";
-     Statement st = ConnectionWithDatabase.connectWithDatabase().createStatement();
-	 ResultSet rs = st.executeQuery(query);
-	 boolean isFound=false;
-	 
-	 while (rs.next())
-	 {
-	   String name = rs.getString("name");
-	   String email = rs.getString("email");
-	   String password=rs.getString("password");
-	if(name.equals(searchedName) && email.equals(searchedEmail) && password.equals(searchedPassword)) {
-		isFound=true;
-	}
-	
-	} 
-	 if(isFound==true) {
-		 Menu.menuForAdmin();
-	 }
-	 else 
-		 System.out.println("Not found an admin!");
-	  st.close();
-	 }
-	catch (Exception e)
-	{
-	 System.err.println("Got an exception! ");
-	 System.err.println(e.getMessage());
-	}
-	
-	scanner.close();
-	}
-	
-	public static String checkForUser() {
-		System.out.println("Enter name,email and password:");
-		Scanner scanner = new Scanner(System.in);
-		String searchedName = scanner.nextLine();
-	    String searchedEmail = scanner.nextLine();
-	    String searchedPassword = scanner.nextLine();
-	    
-	 try {
-	 String query = "SELECT * FROM users JOIN services ON users.id=services.user_id";
-     Statement st = ConnectionWithDatabase.connectWithDatabase().createStatement();
-	 ResultSet rs = st.executeQuery(query);
-	 
-	 boolean isFound=false;
-	
-	 while (rs.next())
-	 {
-	   String name = rs.getString("name");
-	   String email = rs.getString("email");
-	   String password=rs.getString("password");
-	   String typeService = rs.getString("typeService");
-	   Date end = rs.getDate("end");
-	   System.out.println("Service:");
-	   System.out.println(typeService);
-	   System.out.println("UserName:");
-	   System.out.println(name);
-	   System.out.println("You can pay for the services at the latest by:");
-	   System.out.println(end);
-	  if(name.equals(searchedName) && email.equals(searchedEmail) && password.equals(searchedPassword)) {
-		isFound=true;
-	  }
-	  } 
-	 if(isFound==false) {
-	
-		 System.out.println("Not found a user!");
-	 }
-	  st.close();
-	 }
-	catch (Exception e)
-	{
-	 System.err.println("Got an exception! ");
-	 System.err.println(e.getMessage());
+	public static void checkForAdmin(String searchedEmail, String searchedPassword) {
+
+		try {
+
+			String query = "SELECT * FROM admins";
+			Statement st = ConnectionWithDatabase.connectWithDatabase().createStatement();
+			ResultSet rs = st.executeQuery(query);
+			boolean isFound = false;
+
+			while (rs.next()) {
+
+				String email = rs.getString("email");
+				String password = rs.getString("password");
+				if (email.equals(searchedEmail) && password.equals(searchedPassword)) {
+					isFound = true;
+				}
+
+			}
+			if (isFound == true) {
+				Menu.menuForAdmin();
+			} else
+				System.out.println("Not found an admin!");
+			st.close();
+		} catch (Exception e) {
+			System.err.println("Got an exception! ");
+			System.err.println(e.getMessage());
+		}
+
 	}
 
-	scanner.close();
-	return searchedEmail;
+	public static String checkForUser(String searchedEmail, String searchedPassword) {
+
+		try {
+
+			String query = "SELECT * FROM users";
+			Statement st = ConnectionWithDatabase.connectWithDatabase().createStatement();
+			ResultSet rs = st.executeQuery(query);
+			boolean isFound = false;
+
+			while (rs.next()) {
+
+				String email = rs.getString("email");
+				String password = rs.getString("password");
+				if (email.equals(searchedEmail) && password.equals(searchedPassword)) {
+					isFound = true;
+				}
+
+			}
+			if (isFound == true) {
+				try {
+					String queryForService = "SELECT * FROM users JOIN services ON users.id=services.user_id";
+					Statement st1 = ConnectionWithDatabase.connectWithDatabase().createStatement();
+					ResultSet rs1 = st1.executeQuery(queryForService);
+					while (rs1.next()) {
+						String typeService = rs1.getString("typeService");
+						Date end = rs1.getDate("end");
+						String email = rs1.getString("email");
+						if (email.equals(searchedEmail)) {
+							System.out.println("Service:");
+							System.out.println(typeService);
+							System.out.println("You can pay for the services at the latest by:");
+							System.out.println(end);
+						} else
+							System.out.println("No service");
+					}
+				} catch (Exception e) {
+					System.err.println("Got an exception! ");
+					System.err.println(e.getMessage());
+				}
+			} else
+				System.out.println("Not found!");
+			st.close();
+		} catch (Exception e) {
+			System.err.println("Got an exception! ");
+			System.err.println(e.getMessage());
 		}
+
+		return searchedEmail;
+	}
 }
