3642956eafdf2fd8bc3bb2eab5acbe33b90b20eb
Anna-MariaFilipova <58307360+Anna-MariaFilipova@users.noreply.github.com>
2020-07-13 17:02:47 +0300

    Update Service.java

--- Service.java
+++ Service.java
@@ -1,71 +1,119 @@
 package mobileOperator;
-public class Service {     	
-private int id;	
 
-private String typeService ;
-private double	price;
-private int userId;
-private String end ; 
+public class Service {
 
-private boolean isActivated;
+	private int id;
+	private String typeService;
+	private String startService;
+	private String endService;
+	private String lastPaymentDate;
+	private int countMinutes;
+	private int countSMS;
+	private int countMegabytes;
+	private double price;
+	private int userId;
+	private boolean isActivated;
 
-Service(String typeService,double price, String end,int userId,boolean isActivated){
-	this.typeService=typeService;
-	this.price=price;
-	this.end=end;
-	this.userId=userId;
-	this.isActivated=isActivated;
-}
+	Service(String typeService, String endService, String lastPaymentDate, int countMinutes, int countSMS,
+			int countMegabytes, double price, int userId, boolean isActivated) {
+		this.typeService = typeService;
+		this.endService = endService;
+		this.lastPaymentDate = lastPaymentDate;
+		this.countMinutes = countMinutes;
+		this.countSMS = countSMS;
+		this.countMegabytes = countMegabytes;
+		this.price = price;
+		this.userId = userId;
+		this.isActivated = isActivated;
+	}
 
-public  void setIsActivated(boolean isActivated) {
-	this.isActivated=isActivated;
-}
+	public void setCountMinutes(int countMinutes) {
+		this.countMinutes = countMinutes;
+	}
 
-public void setId(int id) {
-	this.id=id;
-}
-public void setUserId(int userId) {
-	this.userId=userId;
-}
-public void setTypeService(String typeService) {
- this.typeService=typeService;
-}
+	public void setCountSMS(int countSMS) {
+		this.countSMS = countSMS;
+	}
 
-public void setPrice(double price) {
-	this.price=price;
-}
+	public void setCountMegabytes(int countMegabytes) {
+		this.countMegabytes = countMegabytes;
+		;
+	}
 
-public void setEnd(String end) {
-	this.end=end;
-}
+	public void setStartService(String startService) {
+		this.startService = startService;
+	}
 
+	public void setEndService(String endService) {
+		this.endService = endService;
+	}
 
+	public void setLastPaymentDate(String lastPaymentDate) {
+		this.lastPaymentDate = lastPaymentDate;
+	}
 
-public int getId() {
-	return id;
-}
+	public void setIsActivated(boolean isActivated) {
+		this.isActivated = isActivated;
+	}
 
-public String getTypeService() {
-	return typeService;
-}
+	public void setId(int id) {
+		this.id = id;
+	}
 
-public double getPrice() {
-	return price;
-}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 
-public int getUserId()
-{
-	return userId;
-}
+	public void setTypeService(String typeService) {
+		this.typeService = typeService;
+	}
 
-public String getEnd() {
-	return end;
-}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
+	public int getId() {
+		return id;
+	}
 
+	public String getTypeService() {
+		return typeService;
+	}
 
-public boolean getIsActivated() {
-	return isActivated;
-}
+	public double getPrice() {
+		return price;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public String getLastPaymentDate() {
+		return lastPaymentDate;
+	}
+
+	public int getCountMinutes() {
+		return countMinutes;
+	}
+
+	public int getCountSMS() {
+		return countSMS;
+	}
+
+	public int getCountMegabytes() {
+		return countMegabytes;
+	}
+
+	public String getStartService() {
+		return startService;
+	}
+
+	public String getEndService() {
+		return endService;
+	}
+
+	public boolean getIsActivated() {
+		return isActivated;
+	}
 
 }
