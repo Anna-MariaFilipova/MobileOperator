f5ce021c3950b34f5bb9072add865de6a27190e1
Anna-MariaFilipova <58307360+Anna-MariaFilipova@users.noreply.github.com>
2020-07-14 13:29:15 +0300

    Update ConnectionWithDatabase.java

--- ConnectionWithDatabase.java
+++ ConnectionWithDatabase.java
@@ -6,24 +6,23 @@ import java.sql.DriverManager;
 public class ConnectionWithDatabase {
 	public static Connection connectWithDatabase() {
 
-	String myDriver=null;
-    Connection conn = null;
-    String myUrl=null;
-    
-	try {
-     myDriver="com.mysql.cj.jdbc.Driver";
-	 myUrl="jdbc:mysql://localhost:3307/mobileOperator?serverTimezone=UTC";
-    try {
-       Class.forName(myDriver);
-       } catch (Exception e) {
-       System.out.println(e);
-       }
- conn = (Connection) DriverManager.getConnection(myUrl, "root", "ankabace123");
-}catch (Exception e)
- {
-    System.err.println("Got an exception!");
-    System.err.println(e.getMessage());
-  }
- return conn;
-}
+		String myDriver = null;
+		Connection conn = null;
+		String myUrl = null;
+
+		try {
+			myDriver = "com.mysql.cj.jdbc.Driver";
+			myUrl = "jdbc:mysql://localhost:3307/mobileOperator?serverTimezone=UTC";
+			try {
+				Class.forName(myDriver);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			conn = (Connection) DriverManager.getConnection(myUrl, "root", "ankabace123");
+		} catch (Exception e) {
+			System.err.println("Got an exception!");
+			System.err.println(e.getMessage());
+		}
+		return conn;
+	}
 }
