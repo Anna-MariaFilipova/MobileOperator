d3966e23cd92b2bf42425c76f4a94121e04415b3
Anna-MariaFilipova <58307360+Anna-MariaFilipova@users.noreply.github.com>
2020-07-14 13:34:12 +0300

    Update ServiceStorage.java

--- ServiceStorage.java
+++ ServiceStorage.java
@@ -1,49 +1,53 @@
 package mobileOperator;
 
 import java.sql.PreparedStatement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Scanner;
 
 public class ServiceStorage {
 
-	public static Service createService() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter typeService,end of service,last payment date,count minutes,count sms,count megabytes, price,userId and isActivated:");
-		String typeService = scanner.nextLine();
-		String endService = scanner.nextLine();
-		String lastPaymentDate = scanner.nextLine();
-		int countMinutes = scanner.nextInt();
-		int countSMS = scanner.nextInt();
-		int countMegabytes = scanner.nextInt();
-		double price = scanner.nextDouble();
-		int userId = scanner.nextInt();
-		boolean isActivated = scanner.nextBoolean();
-		
+	public static Service createService(String typeService, String endServiceStr, String lastPaymentDateStr,
+			int countMinutes, int countSMS, int countMegabytes, double price, int userId, boolean isActivated) {
+
+		java.util.Date lastPaymentDateUtil = null;
+		java.util.Date endServiceUtil = null;
+		java.sql.Date endService = null;
+		java.sql.Date lastPaymentDate = null;
+		try {
+			lastPaymentDateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(lastPaymentDateStr);
+			endServiceUtil = new SimpleDateFormat("yyyy-MM-dd").parse(endServiceStr);
+			lastPaymentDate = new java.sql.Date(lastPaymentDateUtil.getTime());
+			endService = new java.sql.Date(endServiceUtil.getTime());
+			;
+		} catch (ParseException e) {
+
+			e.printStackTrace();
+		}
+
 		Service service = new Service(typeService, endService, lastPaymentDate, countMinutes, countSMS, countMegabytes,
 				price, userId, isActivated);
-		scanner.close();
+
 		return service;
 	}
 
 	public static void setServiceInDatabase(Service service) {
 		Calendar calendar = Calendar.getInstance();
 		java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
-		String query = " insert into services (id,typeService,price,startService,endService,lastPymentDate,countMinutes,countSMS,countMegabytes,user_id,isActivated)"
-				+ " values (?,?,?,?,?,?,?,?,?,?,?)";
+		String query = " insert into services (typeService,price,startService,endService,lastPaymentDate,countMinutes,countSMS,countMegabytes,user_id,isActivated)"
+				+ " values (?,?,?,?,?,?,?,?,?,?)";
 		try {
 			PreparedStatement preparedStmt = ConnectionWithDatabase.connectWithDatabase().prepareStatement(query);
-			preparedStmt.setInt(1, service.getId());
-			preparedStmt.setString(2, service.getTypeService());
-			preparedStmt.setDouble(3, service.getPrice());
-			preparedStmt.setDate(4, startDate);
-			preparedStmt.setString(5, service.getEndService());
-			preparedStmt.setString(6, service.getLastPaymentDate());
-			preparedStmt.setInt(7, service.getCountMinutes());
-			preparedStmt.setInt(8, service.getCountSMS());
-			preparedStmt.setInt(9, service.getCountMegabytes());
-		
-			preparedStmt.setInt(10, service.getUserId());
-			preparedStmt.setBoolean(11, service.getIsActivated());
+			preparedStmt.setString(1, service.getTypeService());
+			preparedStmt.setDouble(2, service.getPrice());
+			preparedStmt.setDate(3, startDate);
+			preparedStmt.setDate(4, service.getEndService());
+			preparedStmt.setDate(5, service.getLastPaymentDate());
+			preparedStmt.setInt(6, service.getCountMinutes());
+			preparedStmt.setInt(7, service.getCountSMS());
+			preparedStmt.setInt(8, service.getCountMegabytes());
+			preparedStmt.setInt(9, service.getUserId());
+			preparedStmt.setBoolean(10, service.getIsActivated());
 			preparedStmt.execute();
 			System.out.println("Record is inserted in the table successfully");
 		} catch (Exception e) {
@@ -51,5 +55,4 @@ public class ServiceStorage {
 			System.err.println(e.getMessage());
 		}
 	}
-
 }
