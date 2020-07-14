76cdc1985c6f52ab2d919bb2456cd057f7a35c3e
Anna-MariaFilipova <58307360+Anna-MariaFilipova@users.noreply.github.com>
2020-07-14 13:27:29 +0300

    Update AdminOperations.java

--- AdminOperations.java
+++ AdminOperations.java
@@ -2,47 +2,77 @@ package mobileOperator;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Date;
+import java.util.Calendar;
+import java.sql.Date;
 
 public class AdminOperations {
 
-	public static void showServiceByUserName(String searchedname) {
-
+	public static void searchServiceByEmail(String searchedEmail) {
 		try {
-			String query = "SELECT users.name,users.email,services.typeService,services.end FROM users JOIN services ON users.id=services.user_id";
+			String query = "SELECT users.name,users.email,services.countSMS,services.countMinutes,services.countMegabytes,services.typeService,services.lastPaymentDate,services.endService FROM services JOIN users\r\n"
+					+ "ON users.id=services.user_id\r\n";
+
 			Statement st = ConnectionWithDatabase.connectWithDatabase().createStatement();
 			ResultSet rs = st.executeQuery(query);
 			while (rs.next()) {
 				String name = rs.getString("name");
+				String email = rs.getString("email");
 				String typeService = rs.getString("typeService");
-				Date end = rs.getDate("end");
-				if (name.equals(searchedname)) {
-					System.out.format("%s, %s, %s\n", name, typeService, end);
-				}
+				int smsCount = rs.getInt("countSMS");
+				int minutesCount = rs.getInt("countMinutes");
+				int megabytesCount = rs.getInt("countMegabytes");
+				Date lastPaymentDate = rs.getDate("lastPaymentDate");
+				Date endOfService = rs.getDate("endService");
+				if (email.equals(searchedEmail)) {
+					System.out.format("%s, %s, %s\n", name, email, typeService);
+					System.out.println("Minutes:");
+					System.out.println(minutesCount);
+					System.out.println("SMS:");
+					System.out.println(smsCount);
+					System.out.println("megabytes:");
+					System.out.println(megabytesCount);
+					System.out.println("last payment date:");
+					System.out.println(lastPaymentDate);
+					System.out.println("end of service:");
+					System.out.println(endOfService);
+
+				} else
+					System.out.println("Not found user");
+
 			}
 			st.close();
 		} catch (Exception e) {
 			System.err.println("Got an exception! ");
 			System.err.println(e.getMessage());
 		}
-
 	}
 
-	public static void showUserNameByTypeOfService(String searchedTypeService) {
+	public static void searchUserByService(String searchedTypeService) {
 
 		try {
+			String query = "SELECT users.name,users.email,services.countSMS,services.countMinutes,services.countMegabytes,services.typeService,services.lastPaymentDate FROM services JOIN users\r\n"
+					+ "ON users.id=services.user_id\r\n";
 
-			String query = "SELECT users.name,users.email,services.typeService,services.end FROM users JOIN services ON users.id=services.user_id";
 			Statement st = ConnectionWithDatabase.connectWithDatabase().createStatement();
 			ResultSet rs = st.executeQuery(query);
 			while (rs.next()) {
 				String name = rs.getString("name");
+				String email = rs.getString("email");
 				String typeService = rs.getString("typeService");
-				Date end = rs.getDate("end");
+				int smsCount = rs.getInt("countSMS");
+				int minutesCount = rs.getInt("countMinutes");
+				int megabytesCount = rs.getInt("countMegabytes");
 				if (typeService.equals(searchedTypeService)) {
+					System.out.format("%s, %s, %s\n", name, email, typeService);
+					System.out.println("Minutes:");
+					System.out.println(minutesCount);
+					System.out.println("SMS:");
+					System.out.println(smsCount);
+					System.out.println("megabytes:");
+					System.out.println(megabytesCount);
+				} else
+					System.out.println("Not found service");
 
-					System.out.format("%s, %s, %s\n", name, typeService, end);
-				}
 			}
 			st.close();
 		} catch (Exception e) {
@@ -52,17 +82,62 @@ public class AdminOperations {
 
 	}
 
-	public static void unpaidServices() {
+	public static void showUnpaidServices() {
 		try {
-			String query = "SELECT users.name,services.typeService,services.lastPaymentDate FROM services JOIN users\r\n"
+			String query = "SELECT users.name,users.email,services.countSMS,services.countMinutes,services.countMegabytes,services.typeService,services.lastPaymentDate FROM services JOIN users\r\n"
 					+ "ON users.id=services.user_id\r\n" + "WHERE services.lastPaymentDate<now();";
 			Statement st = ConnectionWithDatabase.connectWithDatabase().createStatement();
 			ResultSet rs = st.executeQuery(query);
 			while (rs.next()) {
 				String name = rs.getString("name");
+				String email = rs.getString("email");
 				String typeService = rs.getString("typeService");
-				Date end = rs.getDate("end");
-				System.out.format("%s, %s, %s\n", name, typeService, end);
+				int smsCount = rs.getInt("countSMS");
+				int minutesCount = rs.getInt("countMinutes");
+				int megabytesCount = rs.getInt("countMegabytes");
+				System.out.format("%s, %s, %s\n", name, email, typeService);
+				System.out.println("Minutes:");
+				System.out.println(minutesCount);
+				System.out.println("SMS:");
+				System.out.println(smsCount);
+				System.out.println("megabytes:");
+				System.out.println(megabytesCount);
+
+			}
+			st.close();
+		} catch (Exception e) {
+			System.err.println("Got an exception! ");
+			System.err.println(e.getMessage());
+		}
+	}
+
+	public static void isActive() {
+		Calendar calendar = Calendar.getInstance();
+		java.sql.Date datenow = new java.sql.Date(calendar.getTime().getTime());
+		try {
+			String query = "SELECT users.name,users.email,services.countSMS,services.countMinutes,services.countMegabytes,services.typeService,services.lastPaymentDate,services.isActivated,services.endService FROM services JOIN users\r\n"
+					+ "ON users.id=services.user_id\r\n";
+
+			Statement st = ConnectionWithDatabase.connectWithDatabase().createStatement();
+			ResultSet rs = st.executeQuery(query);
+			while (rs.next()) {
+
+				String typeService = rs.getString("typeService");
+				Date lastPaymentDate = rs.getDate("lastPaymentDate");
+				Date endService = rs.getDate("endService");
+				int smsCount = rs.getInt("countSMS");
+				int minutesCount = rs.getInt("countMinutes");
+				int megabytesCount = rs.getInt("countMegabytes");
+				boolean isActive = rs.getBoolean("isActivated");
+				Service service = new Service(typeService, lastPaymentDate, endService, smsCount, minutesCount,
+						megabytesCount, isActive);
+				if (datenow.compareTo(lastPaymentDate) > 0 || datenow.compareTo(endService) > 0) {
+					service.setIsActivated(false);
+				}
+				if (smsCount == 0 && minutesCount == 0 && megabytesCount == 0) {
+					service.setIsActivated(false);
+				}
+
 			}
 			st.close();
 		} catch (Exception e) {
