ad06fdcaeab408bebf734ce6623e5a71d024dfd6
Anna-MariaFilipova <58307360+Anna-MariaFilipova@users.noreply.github.com>
2020-07-14 13:29:48 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
 package mobileOperator;
 
-import java.util.Scanner;
-public class Main   {
-	
-public static void main(String[] args) {
-	
-	Menu.mainMenu();
-}
+public class Main {
 
+	public static void main(String[] args) {
 
-
-
+		AdminOperations.isActive(); /*
+									 * Проверява дали са изпълнени условията услугата да е активна ,ако не са
+									 * изпълнени услугата се деактивира
+									 */
+		Menu.mainMenu();
+	}
 
 }
 
