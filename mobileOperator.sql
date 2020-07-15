3e9625d07b63f05676c47c7a24b37d04ac036a56
Anna-MariaFilipova <58307360+Anna-MariaFilipova@users.noreply.github.com>
2020-07-15 14:41:11 +0300

    Update mobileOperator.sql

--- mobileOperator.sql
+++ mobileOperator.sql
@@ -18,17 +18,19 @@ password VARCHAR(255)         NOT NULL,
 date_created timestamp default now()
 );
 
-CREATE TABLE services(
-id              INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-typeService     VARCHAR(255)        NOT NULL,
-price           DOUBLE              NOT NULL,
-startService    DATETIME            NOT NULL,
-endService      DATETIME            NOT NULL,
-lastPaymentDate DATETIME NOT NULL,
-countMinutes    INT                 NOT NULL,
-countSMS        INT                 NOT NULL,
-countMegabytes  INT                 NOT NULL,
-user_id         INT                 NOT NULL,
-isActivated boolean                 NOT NULL,
-CONSTRAINT FOREIGN KEY(user_id) REFERENCES users(id)
+CREATE TABLE services (
+    id              INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    typeService     VARCHAR(255)       NOT NULL,
+    price           DOUBLE             NOT NULL,
+    startService    DATE           NOT NULL,
+    endService      DATE           NOT NULL,
+    lastPaymentDate DATE           NOT NULL,
+    countMinutes    INT                NOT NULL,
+    countSMS        INT                NOT NULL,
+    countMegabytes  INT                NOT NULL,
+    user_id         INT                NOT NULL,
+    isActivated     BOOLEAN            NOT NULL,
+    CONSTRAINT FOREIGN KEY (user_id)
+        REFERENCES users (id)
 );
+
