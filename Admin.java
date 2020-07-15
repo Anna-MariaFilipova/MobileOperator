986551b6906c85e822576aaf8c922a97efaedc7c
Anna-MariaFilipova <58307360+Anna-MariaFilipova@users.noreply.github.com>
2020-07-15 14:23:51 +0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,7 +1,7 @@
 package mobileOperator;
 
 public class Admin {
-
+	private int id;
 	private String name;
 	private String email;
 	private String password;
@@ -16,16 +16,25 @@ public class Admin {
 		this.name = name;
 	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
 	public void setPassword(String password) {
+
 		this.password = password;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 	public String getEmail() {
