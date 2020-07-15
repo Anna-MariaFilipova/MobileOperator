696ee0d57f5961596310503cc604e8fa37ed840d
Anna-MariaFilipova <58307360+Anna-MariaFilipova@users.noreply.github.com>
2020-07-15 14:35:27 +0300

    Update Register.java

--- Register.java
+++ Register.java
@@ -1,47 +1,64 @@
 package mobileOperator;
 
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Calendar;
 
 public class Register {
 
-	public static void setUserInDatabase(String name, String email, String password) {
+	public void setUserInDatabase(String name, String email, String password) throws PasswordException, EmailException {
+		String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+		if (email.matches(regex) == false) {
+			throw new EmailException();
+		}
+		if (password.length() < 8)
+			throw new PasswordException();
 		User user = new User(name, email, password);
 		Calendar calendar = Calendar.getInstance();
 		java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
 		String query = " insert into users (name,email,date_created,password)" + " values (?, ?, ?,?)";
+		PreparedStatement preparedStmt;
 		try {
-			PreparedStatement preparedStmt = ConnectionWithDatabase.connectWithDatabase().prepareStatement(query);
+			preparedStmt = ConnectionWithDatabase.connectWithDatabase().prepareStatement(query);
 			preparedStmt.setString(1, user.getName());
 			preparedStmt.setString(2, user.getEmail());
 			preparedStmt.setDate(3, startDate);
 			preparedStmt.setString(4, user.getPassword());
 			preparedStmt.execute();
 			System.out.println("Record is inserted in the table successfully");
+		} catch (SQLException e) {
+			System.out.println("Cannot connect to the database");
 		} catch (Exception e) {
-			System.err.println("Got an exception!");
-			System.err.println(e.getMessage());
+			System.err.println("Got an exception in adding user in database!");
 		}
 	}
 
-	public static void setAdminInDatabase(String name, String email, String password) {
+	public void setAdminInDatabase(String name, String email, String password)
+			throws PasswordException, EmailException {
+		String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+		if (email.matches(regex) == false) {
+			throw new EmailException();
+		}
+		if (password.length() < 8)
+			throw new PasswordException();
 		Admin admin = new Admin(name, email, password);
 		Calendar calendar = Calendar.getInstance();
 		java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
 		String query = " insert into admins (name,email,password,datecreated)" + " values (?, ?, ?,?)";
+		PreparedStatement preparedStmt;
 		try {
-			PreparedStatement preparedStmt = ConnectionWithDatabase.connectWithDatabase().prepareStatement(query);
+			preparedStmt = ConnectionWithDatabase.connectWithDatabase().prepareStatement(query);
 			preparedStmt.setString(1, admin.getName());
 			preparedStmt.setString(2, admin.getEmail());
 			preparedStmt.setString(3, admin.getPassword());
 			preparedStmt.setDate(4, startDate);
 			preparedStmt.execute();
 			System.out.println("Record is inserted in the table successfully");
-		}
+		} catch (SQLException e) {
+			System.out.println("Cannot connect to the database");
+		} catch (Exception e) {
+			System.err.println("Got an exception in adding admin in database!");
 
-		catch (Exception e) {
-			System.err.println("Got an exception!");
-			System.err.println(e.getMessage());
 		}
 	}
 
