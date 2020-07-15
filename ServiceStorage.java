16d69976dfa198cde6effa69e066b23a6382d0c2
Anna-MariaFilipova <58307360+Anna-MariaFilipova@users.noreply.github.com>
2020-07-15 14:37:11 +0300

    Update ServiceStorage.java

--- ServiceStorage.java
+++ ServiceStorage.java
@@ -1,15 +1,18 @@
 package mobileOperator;
 
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class ServiceStorage {
 
-	public static Service createService(String typeService, String endServiceStr, String lastPaymentDateStr,
-			int countMinutes, int countSMS, int countMegabytes, double price, int userId, boolean isActivated) {
-
+	public static Service createService(String serviceName, String endServiceStr, String lastPaymentDateStr,
+			int countMinutes, int countSMS, int countMegabytes, double price, int userId, boolean isActivated)
+			throws PriceException {
+		if (price < 0)
+			throw new PriceException();
 		java.util.Date lastPaymentDateUtil = null;
 		java.util.Date endServiceUtil = null;
 		java.sql.Date endService = null;
@@ -21,27 +24,25 @@ public class ServiceStorage {
 			endService = new java.sql.Date(endServiceUtil.getTime());
 			;
 		} catch (ParseException e) {
-
-			e.printStackTrace();
+			System.out.println("Cannot convert string to date.");
 		}
-
-		Service service = new Service(typeService, endService, lastPaymentDate, countMinutes, countSMS, countMegabytes,
+		Service service = new Service(serviceName, endService, lastPaymentDate, countMinutes, countSMS, countMegabytes,
 				price, userId, isActivated);
 
 		return service;
 	}
 
-	public static void setServiceInDatabase(Service service) {
+	public void setServiceInDatabase(Service service) {
 		Calendar calendar = Calendar.getInstance();
 		java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
 		String query = " insert into services (typeService,price,startService,endService,lastPaymentDate,countMinutes,countSMS,countMegabytes,user_id,isActivated)"
 				+ " values (?,?,?,?,?,?,?,?,?,?)";
 		try {
 			PreparedStatement preparedStmt = ConnectionWithDatabase.connectWithDatabase().prepareStatement(query);
-			preparedStmt.setString(1, service.getTypeService());
+			preparedStmt.setString(1, service.getServiceName());
 			preparedStmt.setDouble(2, service.getPrice());
 			preparedStmt.setDate(3, startDate);
-			preparedStmt.setDate(4, service.getEndService());
+			preparedStmt.setDate(4, service.getServiceEnd());
 			preparedStmt.setDate(5, service.getLastPaymentDate());
 			preparedStmt.setInt(6, service.getCountMinutes());
 			preparedStmt.setInt(7, service.getCountSMS());
@@ -50,9 +51,10 @@ public class ServiceStorage {
 			preparedStmt.setBoolean(10, service.getIsActivated());
 			preparedStmt.execute();
 			System.out.println("Record is inserted in the table successfully");
-		} catch (Exception e) {
-			System.err.println("Got an exception!");
-			System.err.println(e.getMessage());
+		} catch (NullPointerException e) {
+			System.out.println("Cannot save null in database");
+		} catch (SQLException e) {
+			System.out.println("Cannot connect to the database");
 		}
 	}
 }
